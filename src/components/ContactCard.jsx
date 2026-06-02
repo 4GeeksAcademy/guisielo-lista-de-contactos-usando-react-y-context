e16301049821bdc3e9c6b9b1d11e6c07a9399d7f
Guisielo <155173663+Guisielo@users.noreply.github.com>
2026-06-02 02:22:46 +0000

    Proyecto corregido y mejorado

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,6 +1,6 @@
-import useGlobalReducer from "../hooks/useGlobalReducer";
-import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer";
 
 export const ContactCard = ({ contact }) => {
   const { dispatch } = useGlobalReducer();
@@ -9,44 +9,50 @@ export const ContactCard = ({ contact }) => {
 
   const deleteContact = async () => {
 
-    await fetch(
-      `https://playground.4geeks.com/contact/agendas/guisielo/contacts/${contact.id}`,
-      {
-        method: "DELETE"
+    try {
+      const resp = await fetch(
+        `https://playground.4geeks.com/contact/agendas/guisielo/contacts/${contact.id}`,
+        {
+          method: "DELETE"
+        }      
+      );
+
+      if (!resp.ok) {
+        throw new Error(`Error HTTP: ${resp.status}`);
       }
-    );
 
-    dispatch({
-      type: "DELETE_CONTACT",
-      payload: contact.id
-    });
-  };
-
-  const goToEdit = () => {
-    navigate(`/edit-contact/${contact.id}`);
+      dispatch({
+        type: "DELETE_CONTACT",
+        payload: contact.id
+      });
+  
+    } catch (error) {
+        console.error(error);
+        alert("No se pudo eliminar el contacto");
+    }
   };
 
   return (
     <>
     <div className="card mb-3" >
       <div className="row g-0">
-        <div className="col-md-3 d-flex justify-content-center align-items-center">
+        <div className="col-md-3 contact-image-container">
           <img
             src="https://images.pexels.com/photos/35725756/pexels-photo-35725756.jpeg"
             className="img-fluid rounded-circle"
-            alt="..."
+            alt={contact.name}
           />
         </div>
         <div className="col-md-7">
           <div className="card-body">
             <h5 className="card-title">{contact.name}</h5>
-            <p className="card-text"><small className="text-muted"><i class="fa-solid fa-location-dot me-2"></i>{contact.address}</small></p>
-            <p className="card-text"><small className="text-muted"><i class="fa-solid fa-phone me-2"></i>{contact.phone}</small></p>
-            <p className="card-text"><small className="text-muted"><i class="fa-solid fa-envelope me-2"></i>{contact.email}</small></p>
+            <p className="card-text"><small className="text-muted"><i className="fa-solid fa-location-dot me-2"></i>{contact.address}</small></p>
+            <p className="card-text"><small className="text-muted"><i className="fa-solid fa-phone me-2"></i>{contact.phone}</small></p>
+            <p className="card-text"><small className="text-muted"><i className="fa-solid fa-envelope me-2"></i>{contact.email}</small></p>
           </div>
         </div> 
         <div className="col-md-2">
-          <i className="fa-solid fa-pencil me-3" onClick={goToEdit}></i>
+          <i className="fa-solid fa-pencil me-3" onClick={() => navigate(`/edit-contact/${contact.id}`)}></i>
           <i className="fa-solid fa-trash" onClick={() => setShowModal(true)}></i>    
         </div>
       </div>
